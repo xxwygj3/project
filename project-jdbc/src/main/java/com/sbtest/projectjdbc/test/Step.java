e418b255db5f7162bb33fe7b48faa0c7c03e15f6
gjye <gangjie.ye@joinwe.com.cn>
2018-01-30 16:31:46 +0800

    solver MazeTest

---
+++ project-jdbc/src/main/java/com/sbtest/projectjdbc/test/Step.java
@@ -0,0 +1,10 @@
+package com.sbtest.projectjdbc.test;
+
+public class Step {
+    int x,y,d;
+    public Step(int x,int y,int d) {
+        this.x = x;//横坐标
+        this.y = y;//纵坐标
+        this.d = d;//方向
+    }
+}
